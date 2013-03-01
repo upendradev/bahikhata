c09a6326c30a7412f3711c2009dd899adc41879e
upendradev <udmanit@gmail.com>
2013-03-01 23:52:42 +0530

    added dust template support

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+, dust = require('dustjs-linkedin')
+  , cons = require('consolidate') 
+,path = require('path');
 
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'dust');
+app.engine('dust', cons.dust);
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
